7aaec97db9321a332a03948e36fdc58d52226b73
TheKauanLima <thekauanlima@gmail.com>
2024-11-02 18:59:03 -0400

    Add project files.

---
+++ EconomizzeUserApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EconomizzeUserApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
